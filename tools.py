3c796de84657ecdc67de16e995dbd29562aec54e
Dory <dory@dory.moe>
2025-10-27 16:22:10 -0700

    refactor tools

--- tools.py
+++ tools.py
@@ -1,122 +1,85 @@
-from abc import ABC, abstractmethod
+import collections
 from datetime import datetime
 import functools
 import html2text
+import inspect
 import json
 import os
 import requests
+from pydantic import Field
 
 
-class Tool(ABC):
-  @abstractmethod
-  def get_spec(self):
-    """Returns the dictionary representing function spec for the tool."""
-    ...
-
-  @abstractmethod
-  def run(self, **kwargs):
-    """Executes the tool with the required parameters."""
-    ...
+def get_time():
+  """Get the current local time."""
+  now = datetime.now()
+  return f"{now.strftime("%A")} {now.isoformat()} {now.astimezone().tzinfo}"
 
 
-class Time(Tool):
-  def get_spec(self):
-    return {
-        "type": "function",
-        "function": {
-            "name": "get_time",
-            "description": "Get the current local time.",
-            "parameters": {},
-        },
-    }
-
-  def run(self):
-    now = datetime.now()
-    return f"{now.isoformat()} {now.astimezone().tzinfo}"
+def web_fetch(url: str = Field(..., description="the webpage URL to fetch")):
+  """Get content of a webpage."""
+  if not url.startswith(("http://", "https://")):
+    url = "https://" + url
+  webres = requests.get(url)
+  webres.raise_for_status()
+  return html2text.html2text(webres.text)
 
 
-class WebFetch(Tool):
-  def get_spec(self):
-    return {
-        "type": "function",
-        "function": {
-            "name": "web_fetch",
-            "description": "Get content of a webpage.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "url": {
-                        "type": "string",
-                        "description": "the webpage URL to fetch"
-                    }
-                },
-                "required": ["url"],
-            },
-        },
-    }
-
-  def run(self, url):
-    if not url.startswith(("http://", "https://")):
-      url = "https://" + url
-    webres = requests.get(url)
-    webres.raise_for_status()
-    return html2text.html2text(webres.text)
-
-
-class WebSearch(Tool):
-  def get_spec(self):
-    return {
-        "type": "function",
-        "function": {
-            "name": "web_search",
-            "description": "Search the web.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "query": {
-                        "type": "string",
-                        "description": "the web search query"
-                    },
-                    "num_results": {
-                        "type": "integer",
-                        "description": "how many sites to return. Default is 5"
-                    }
-                },
-                "required": ["query"],
-            },
-        },
-    }
-
-  def run(self, query, num_results=5):
-    res = requests.post(
-        "https://api.langsearch.com/v1/web-search",
-        json={"query": query, "summary": True, "count": num_results},
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {os.environ.get("LANGSEARCH_API_KEY")}",
-        },
-    ).json()
-    cleaned_res = [
-        {
-            "name": pg["name"],
-            "url": pg["url"],
-            "summary": pg["summary"] or pg["snippet"],
-        }
-        for pg in res["data"]["webPages"]["value"]
-    ]
-    return json.dumps(cleaned_res)
+def web_search(
+    query: str = Field(..., description="the web search query"),
+    num_results: int = Field(5, description="how many pages to get. Default 5"),
+):
+  """Search the web."""
+  res = requests.post(
+      "https://api.langsearch.com/v1/web-search",
+      json={"query": query, "summary": True, "count": num_results},
+      headers={
+          "Content-Type": "application/json",
+          "Authorization": f"Bearer {os.environ.get("LANGSEARCH_API_KEY")}",
+      },
+  ).json()
+  cleaned_res = [
+      {
+          "name": pg["name"],
+          "url": pg["url"],
+          "summary": pg["summary"] or pg["snippet"],
+      }
+      for pg in res["data"]["webPages"]["value"]
+  ]
+  return json.dumps(cleaned_res)
 
 
 class Tools:
+  TYPES = collections.defaultdict(lambda: "string", {int: "integer"})
+
   def __init__(self):
-    tools = [cls() for cls in Tool.__subclasses__()]
-    self._tools = {t.get_spec()["function"]["name"]: t for t in tools}
+    self._tools = {f.__name__: f for f in [get_time, web_fetch, web_search]}
 
   @functools.cache
   def tools(self):
-    return [tool.get_spec() for tool in self._tools.values()]
+    return [self._get_spec(f) for f in self._tools.values()]
+
+  def _get_spec(self, f):
+    spec = {
+        "type": "function",
+        "function": {
+            "name": f.__name__,
+            "description": inspect.getdoc(f),
+            "parameters": {"type": "object", "properties": {}, "required": []},
+        },
+    }
+    for name, p in inspect.signature(f).parameters.items():
+      prop = {}
+      prop["type"] = self.TYPES[p.annotation]
+      prop["description"] = p.default.description
+      if p.default.is_required():
+        spec["function"]["parameters"]["required"].append(name)
+      spec["function"]["parameters"]["properties"][name] = prop
+    if not spec["function"]["parameters"]["properties"]:
+      spec["function"]["parameters"] = {}
+    return spec
 
   def call(self, method, **kwargs):
     if method not in self._tools:
       raise ValueError(f"unknown method: {method}")
-    return self._tools[method].run(**kwargs)
+    return self._tools[method](**kwargs)
+
